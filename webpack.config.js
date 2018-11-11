8132549d735ad3bfb51d5b32374a1813a0e7846b
Tatiana Androsova <androsovatati@mail.ru>
2018-11-11 22:02:37 +0400

    Add styles from test task

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
   },
   module: {
     rules: [
@@ -13,14 +14,52 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: "svg-url-loader",
+          options: {}
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[path][name].[ext]?hash=[hash:20]",
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        loader: "file-loader",
+        options: {
+          name: "fonts/[name].[ext]"
         }
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html"
     })
   ]
 };
