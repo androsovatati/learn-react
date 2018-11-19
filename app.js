540940b8c0a6b0f42e6fa412425662b7ba7d38d8
Tatiana Androsova <androsovatati@mail.ru>
2018-11-19 19:14:43 +0400

    Add handlers for events

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
@@ -31,18 +31,23 @@ var isFalse = function isFalse(value) {
 var Input = function Input(props) {
   var opts = _objectSpread({}, props);
 
+  delete opts.onChange;
+
   if (isFalse(opts.required)) {
     delete opts.required;
   }
 
   return React.createElement("input", _extends({
     className: "login-form__input"
-  }, opts));
+  }, opts, {
+    onChange: props.onChange
+  }));
 };
 
 var Button = function Button(props) {
   return React.createElement("button", {
-    className: "login-form__button"
+    className: "login-form__button",
+    onClick: props.onClick
   }, props.children);
 };
 
@@ -58,29 +63,75 @@ var LoginPage = function LoginPage(props) {
   })), props.children);
 };
 
-var LoginForm = function LoginForm(props) {
-  return React.createElement("form", {
-    className: "login-page__form login-form"
-  }, React.createElement("div", {
-    className: "login-form__title"
-  }, "Log in"), React.createElement(Input, {
-    className: "login-form__input",
-    name: "email",
-    type: "email",
-    placeholder: "E-Mail",
-    required: true
-  }), React.createElement(Input, {
-    className: "login-form__input",
-    name: "password",
-    type: "password",
-    placeholder: "Password",
-    required: true
-  }), React.createElement("div", {
-    className: "login-form__error"
-  }), React.createElement(Button, {
-    className: "login-form__button"
-  }, "Login"));
-};
+var LoginForm =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(LoginForm, _React$Component);
+
+  function LoginForm(props) {
+    var _this;
+
+    _classCallCheck(this, LoginForm);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(LoginForm).call(this, props));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "onInputChange", function (e) {
+      _this.setState({
+        name: e.target.value
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "onEmailChange", function (e) {
+      _this.setState({
+        email: e.target.value
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "login", function () {
+      console.log('login', _this.state);
+    });
+
+    _this.state = {
+      name: '',
+      email: ''
+    };
+    return _this;
+  }
+
+  _createClass(LoginForm, [{
+    key: "render",
+    value: function render() {
+      return React.createElement("form", {
+        className: "login-page__form login-form"
+      }, React.createElement("div", {
+        className: "login-form__title"
+      }, "Log in"), React.createElement(Input, {
+        className: "login-form__input",
+        name: "email",
+        type: "email",
+        placeholder: "E-Mail",
+        required: true,
+        value: this.state.name,
+        onChange: this.onInputChange
+      }), React.createElement(Input, {
+        className: "login-form__input",
+        name: "password",
+        type: "password",
+        placeholder: "Password",
+        required: true,
+        value: this.state.email,
+        onChange: this.onEmailChange
+      }), React.createElement("div", {
+        className: "login-form__error"
+      }), React.createElement(Button, {
+        className: "login-form__button",
+        onClick: this.login
+      }, "Login"));
+    }
+  }]);
+
+  return LoginForm;
+}(React.Component);
 
 var LogoutForm = function LogoutForm(props) {
   return React.createElement("form", {
@@ -98,19 +149,19 @@ var LogoutForm = function LogoutForm(props) {
 
 var App =
 /*#__PURE__*/
-function (_React$Component) {
-  _inherits(App, _React$Component);
+function (_React$Component2) {
+  _inherits(App, _React$Component2);
 
   function App(props) {
-    var _this;
+    var _this2;
 
     _classCallCheck(this, App);
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(App).call(this, props));
-    _this.state = {
+    _this2 = _possibleConstructorReturn(this, _getPrototypeOf(App).call(this, props));
+    _this2.state = {
       user: null
     };
-    return _this;
+    return _this2;
   }
 
   _createClass(App, [{
--- app.jsx
+++ app.jsx
@@ -2,14 +2,17 @@ const isFalse = (value) => value === 'false';
 
 const Input = (props) => {
     const opts = { ...props };
+    delete opts.onChange;
     if (isFalse(opts.required)) {
         delete opts.required;
     }
-    return <input className="login-form__input" {...opts} />;
+    return <input className="login-form__input" {...opts} onChange={props.onChange}/>;
 };
 
 const Button = (props) => (
-    <button className="login-form__button">{props.children}</button>
+    <button className="login-form__button" onClick={props.onClick}>
+        {props.children}
+    </button>
 );
 
 const LoginPage = (props) => (
@@ -25,27 +28,61 @@ const LoginPage = (props) => (
     </main>
 );
 
-const LoginForm = (props) => (
-    <form className="login-page__form login-form">
-        <div className="login-form__title">Log in</div>
-        <Input
-            className="login-form__input"
-            name="email"
-            type="email"
-            placeholder="E-Mail"
-            required={true}
-        />
-        <Input
-            className="login-form__input"
-            name="password"
-            type="password"
-            placeholder="Password"
-            required={true}
-        />
-        <div className="login-form__error" />
-        <Button className="login-form__button">Login</Button>
-    </form>
-);
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+        };
+    }
+
+    onInputChange = (e) => {
+        this.setState({
+            name: e.target.value,
+        });
+    }
+
+    onEmailChange = (e) => {
+        this.setState({
+            email: e.target.value,
+        });
+    }
+
+    login = () => {
+        console.log('login', this.state);
+    }
+
+    render() {
+        return (
+            <form className="login-page__form login-form">
+                <div className="login-form__title">Log in</div>
+                <Input
+                    className="login-form__input"
+                    name="email"
+                    type="email"
+                    placeholder="E-Mail"
+                    required={true}
+                    value={this.state.name}
+                    onChange={this.onInputChange}
+                />
+                <Input
+                    className="login-form__input"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required={true}
+                    value={this.state.email}
+                    onChange={this.onEmailChange}
+                />
+                <div className="login-form__error" />
+                <Button className="login-form__button" onClick={this.login}>
+                    Login
+                </Button>
+            </form>
+        );
+    }
+}
 
 const LogoutForm = (props) => (
     <form className="login-page__form logout-form">
