38aadf30c60a65a3e88613e7cfb9a65ffe16789e
Tatiana Androsova <androsovatati@mail.ru>
2018-12-14 15:12:22 +0400

    Change project structure

--- src/app.jsx
+++ src/app.jsx
@@ -1,52 +1,4 @@
-const login = async ({ email, password }) => {
-    const url = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
-    const requestParams = {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify({ email, password }),
-    };
-
-    const response = await fetch(url, requestParams).catch((error) =>
-        Promise.reject({
-            message: 'Network error',
-            body: error,
-        })
-    );
-
-    try {
-        const parsedData = await response.json();
-        if (response.ok) {
-            return parsedData;
-        }
-
-        if (parsedData && parsedData.error) {
-            return Promise.reject({
-                message: parsedData.error,
-                status: response.status,
-                body: parsedData,
-            });
-        }
-
-        let errorMessage = '';
-        if (response.status >= 500) {
-            errorMessage = 'Server error. Try again';
-        } else if (response.status >= 400 && response.status < 500) {
-            errorMessage = 'Application error';
-        }
-
-        return Promise.reject({
-            message: errorMessage || 'Unhandled error',
-            status: response.status,
-            body: parsedData,
-        });
-    } catch (error) {
-        return Promise.reject({
-            message: 'Invalid server data',
-            status: response.status,
-            body: error,
-        });
-    }
-};
+import login from './services/login.js';
 
 const Input = (props) => <input className="login-form__input" {...props} />;
 
@@ -59,7 +11,7 @@ const Button = (props) => (
 const LoginPage = (props) => (
     <main className="login-page">
         <div className="login-page__logo">
-            <img className="company-logo" src="images/w-mercury-development.svg" alt="logo" />
+            <img className="company-logo" src="src/assets/images/w-mercury-development.svg" alt="logo" />
         </div>
         {props.children}
     </main>
---
+++ src/services/login.js
@@ -0,0 +1,49 @@
+export default async ({ email, password }) => {
+  const url = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
+  const requestParams = {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  };
+
+  const response = await fetch(url, requestParams).catch((error) =>
+    Promise.reject({
+      message: 'Network error',
+      body: error,
+    })
+  );
+
+  try {
+    const parsedData = await response.json();
+    if (response.ok) {
+      return parsedData;
+    }
+
+    if (parsedData && parsedData.error) {
+      return Promise.reject({
+        message: parsedData.error,
+        status: response.status,
+        body: parsedData,
+      });
+    }
+
+    let errorMessage = '';
+    if (response.status >= 500) {
+      errorMessage = 'Server error. Try again';
+    } else if (response.status >= 400 && response.status < 500) {
+      errorMessage = 'Application error';
+    }
+
+    return Promise.reject({
+      message: errorMessage || 'Unhandled error',
+      status: response.status,
+      body: parsedData,
+    });
+  } catch (error) {
+    return Promise.reject({
+      message: 'Invalid server data',
+      status: response.status,
+      body: error,
+    });
+  }
+};
